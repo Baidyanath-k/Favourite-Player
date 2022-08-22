e6769bf01617c0246cd790ed93cb87d3308c5575
Baidyanath-K <baidyanathkumar12091998@gmail.com>
2022-08-22 12:38:42 +0600

    add messi and neymar btn and create li list

--- js/apps.js
+++ js/apps.js
@@ -1,1 +1,28 @@
-console.log('ffffffffffffffff')
+function getPlayerName(playerId) {
+    const playerName=document.getElementById(playerId);
+    const playerNameValue=playerName.innerText;
+    return playerNameValue;
+}
+document.getElementById('messi-btn').addEventListener('click',function(){
+    let playerMessi=getPlayerName('messi');
+    let createLi=document.createElement('li');
+    createLi.innerText=playerMessi;
+    let selectedOl=document.getElementById('selected-ol');
+    selectedOl.appendChild(createLi);
+
+    document.getElementById('messi-btn').setAttribute("class", "democlass");
+    document.getElementById('messi-btn').setAttribute("disabled", "");
+    
+})
+
+document.getElementById('neymar-btn').addEventListener('click',function(){
+    let playerNeymar=getPlayerName('neymer');
+    let createLi=document.createElement('li');
+    createLi.innerText=playerNeymar;
+    let selectedOl=document.getElementById('selected-ol');
+    selectedOl.appendChild(createLi);
+
+    document.getElementById('neymar-btn').setAttribute("class", "democlass");
+    document.getElementById('neymar-btn').setAttribute("disabled", "");
+    
+})
